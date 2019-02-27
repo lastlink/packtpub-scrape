2ea76dabbb6d2bdcf526828d29f310f76f3374da
Mr. Funk <funk@Funks-Mac-Pro-4.local>
2019-02-27 01:13:11 -0500

    wip

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const puppeteer = require('puppeteer');
-const creds = require('./config/creds.js');
+const config = require('./config/index.js');
 const fs = require('fs');
 
 
@@ -9,26 +9,53 @@ startTime = new Date();
 
 /** return time elapsed */
 function end() {
-	endTime = new Date();
-	var timeDiff = endTime - startTime; //in ms
-	// strip the ms
-	timeDiff /= 1000;
+    endTime = new Date();
+    var timeDiff = endTime - startTime; //in ms
+    // strip the ms
+    timeDiff /= 1000;
 
-	// get seconds 
-	var seconds = Math.round(timeDiff);
-	// console.log(seconds + " seconds");
-	return "-" + seconds + " seconds";
+    // get seconds 
+    var seconds = Math.round(timeDiff);
+    // console.log(seconds + " seconds");
+    return "-" + seconds + " seconds";
 }
 
 async function run() {
-	const browser = await puppeteer.launch({
-		headless: MAINCONFIG.headless,
-		args: ["--ash-host-window-bounds=1920x1080", "--window-size=1920,1048", "--window-position=0,0"]
-	});
+    const browser = await puppeteer.launch({
+        headless: config.headless,
+        args: ["--ash-host-window-bounds=1920x1080", "--window-size=1920,1080", "--window-position=0,0"]
+    });
 
-	const page = await browser.newPage();
+    const page = await browser.newPage();
+
+    await page.setViewport({ width: 1920, height: 1080 })
 
     console.log("navigating to webpage")
 
     await page.goto('https://www.packtpub.com/packt/offers/free-learning');
+
+    console.log("entering credentials")
+
+    await page.evaluate((config) => {
+        document.querySelector("a.login-popup").click()
+        document.querySelector("div.account-bar-form-left input#email").value = config.email
+        document.querySelector("div.account-bar-form-left input#password").value = config.pw
+    }, config)
+
+    console.log("logging in")
+
+    await page.waitFor(500);
+    await page.evaluate(() => {
+        document.querySelector("#edit-submit-1").click()
+    })
+
+    console.log("claiming book")
+
+    await page.waitFor(500);
+    await page.evaluate(() => {
+        document.querySelector("#fl-claim-btn").click()
+    })
+
 }
+
+run();
